495fec42e2ed0dd10497c6e330dd1cf0dfe5da32
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2025-12-07 12:08:43 +0100

    refactor: move rotation left logic to Direction

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Direction.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Direction.cs
@@ -2,23 +2,51 @@ namespace SmellyMarsRover;
 
 internal record Direction(string Value)
 {
+    private const string WEST = "W";
+    private const string EAST = "E";
+    private const string SOUTH = "S";
+    private const string NORTH = "N";
+
     public bool IsFacingNorth()
     {
-        return this.Value.Equals("N");
+        return this.Value.Equals(NORTH);
     }
 
     public bool IsFacingSouth()
     {
-        return this.Value.Equals("S");
+        return this.Value.Equals(SOUTH);
     }
 
     public bool IsFacingWest()
     {
-        return this.Value.Equals("W");
+        return this.Value.Equals(WEST);  
     }
 
     public static Direction Create(string value)
     {
         return new Direction(value);
+    }
+
+    public Direction RotateLeft()
+    {
+        Direction direction;
+        if (IsFacingNorth())
+        {
+            direction = Create(WEST);
+        }
+        else if (IsFacingSouth())
+        {
+            direction = Create(EAST);
+        }
+        else if (IsFacingWest())
+        {
+            direction = Create(SOUTH);
+        }
+        else
+        {
+            direction = Create(NORTH);
+        }
+
+        return direction;
     }
 }
--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -27,23 +27,7 @@ namespace SmellyMarsRover
 
                 if (command.Equals("l"))
                 {
-                    // Rotate Rover to the left
-                    if (_direction.IsFacingNorth())
-                    {
-                        _direction = Direction.Create("W");
-                    }
-                    else if (_direction.IsFacingSouth())
-                    {
-                        _direction = Direction.Create("E");
-                    }
-                    else if (_direction.IsFacingWest())
-                    {
-                        _direction = Direction.Create("S");
-                    }
-                    else
-                    {
-                        _direction = Direction.Create("N");
-                    }
+                    _direction = _direction.RotateLeft();
                 }
                 else if (command.Equals("r"))
                 {
