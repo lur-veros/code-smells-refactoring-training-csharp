3bb4d4abf041248ef24da80453a2b2f3ea9606f1
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2025-12-07 17:49:42 +0100

    refactor: making fields private

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Coordinates.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Coordinates.cs
@@ -1,14 +1,23 @@
 namespace SmellyMarsRover;
 
-internal record Coordinates(int X, int Y)
+internal record Coordinates
 {
+    private readonly int _x;
+    private readonly int _y;
+
+    public Coordinates(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
+
     public Coordinates MoveAlongYAxis(int displacement)
     {
-        return new Coordinates(this.X, this.Y + displacement);
+        return new Coordinates(_x, _y + displacement);
     }
 
     public Coordinates MoveAlongXAxis(int displacement)
     {
-        return new Coordinates(this.X + displacement, this.Y);
+        return new Coordinates(_x + displacement, _y);
     }
 }
